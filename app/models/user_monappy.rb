2f77dd5280175b01260c1069c01ec59b5dc421d0
Palon <palon7@gmail.com>
2017-05-28 18:06:23 +0900

    Fix user data

--- app/models/user_monappy.rb
+++ app/models/user_monappy.rb
@@ -1,14 +1,14 @@
 class UserMonappy
   def self.sign_in(data)
     user = User.find_or_create_by(
-      email: "#{data['mail']}"
+      email: "#{data.info['mail']}"
     )
     unless user.account.present?
-      user.account = Account.new(username: data[:nickname])
+      user.account = Account.new(username: data.info[:nickname])
       user.password  = Devise.friendly_token[0,20]
       user.skip_confirmation!
     end
-    user.account.avatar_remote_url = data[:image]
+    user.account.avatar_remote_url = data.info[:image]
     user.save!
     user
   end
