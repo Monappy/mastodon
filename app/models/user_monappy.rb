ded1e4d6f80b788fcfc8a419abecd64419a71936
Palon <palon7@gmail.com>
2017-05-29 23:23:04 +0900

    Some fix

--- app/models/user_monappy.rb
+++ app/models/user_monappy.rb
@@ -1,15 +1,21 @@
 class UserMonappy
   def self.sign_in(data)
     user = User.find_or_create_by(
-      email: "#{data.info['mail']}"
+        email: "#{data.info['mail'].downcase}"
     )
+    p user
+    p user.account
     unless user.account.present?
+      p "But not presented..."
       user.account = Account.new(username: data.info[:nickname])
       user.password  = Devise.friendly_token[0,20]
+      user.account.avatar_remote_url = data.info[:image]
+      begin 
+        user.save!
+      rescue
+      end
       user.skip_confirmation!
     end
-    user.account.avatar_remote_url = data.info[:image]
-    user.save!
     user
   end
 
