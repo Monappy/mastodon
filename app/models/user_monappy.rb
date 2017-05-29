c2d23992cd999b909e21a3a5a80537d1f1287ea5
Palon <palon7@gmail.com>
2017-05-30 00:06:34 +0900

    Use uid as login identifier

--- app/models/user_monappy.rb
+++ app/models/user_monappy.rb
@@ -1,13 +1,22 @@
 class UserMonappy
   def self.sign_in(data)
+    temp_user = User.find_by(email: data.info[:mail])
+    p temp_user
+    if temp_user.present? and not temp_user.monappy_uid.present?
+      p "Change..>"
+      temp_user.monappy_uid = data.info[:id]
+      temp_user.save!
+    end
+
     user = User.find_or_create_by(
-        email: "#{data.info['mail'].downcase}"
+        monappy_uid: "#{data.info[:id]}"
     )
     p user
     p user.account
     unless user.account.present?
       p "But not presented..."
       user.account = Account.new(username: data.info[:nickname])
+      user.email = data.info[:mail]
       user.password  = Devise.friendly_token[0,20]
       user.account.avatar_remote_url = data.info[:image]
       begin 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425202925) do
+ActiveRecord::Schema.define(version: 20170529142641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -326,6 +326,7 @@ ActiveRecord::Schema.define(version: 20170425202925) do
     t.boolean  "otp_required_for_login"
     t.datetime "last_emailed_at"
     t.string   "otp_backup_codes",                                       array: true
+    t.integer  "monappy_uid"
     t.index ["account_id"], name: "index_users_on_account_id", using: :btree
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
